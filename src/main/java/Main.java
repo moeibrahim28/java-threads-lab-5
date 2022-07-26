7f686d1a31e52cd8917b1e1c5a573267e926a0c4
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-07-26 11:42:14 -0400

    implemented savings account class using multiple threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+
+        SavingsAccount sa1 = new SavingsAccount();
+        Thread thread1 = new Thread(() -> {
+            sa1.deposit(2000);
+            sa1.withdraw(3000);
+        });
+
+        Thread thread2 = new Thread(() -> {
+            sa1.deposit(1000);
+            sa1.deposit(1000);
+            sa1.withdraw(4000);
+        });
+
+        Thread thread3 = new Thread(() -> {
+            sa1.withdraw(5000);
+            sa1.deposit(10000);
+            sa1.withdraw(5000);
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+        System.out.println(sa1);
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,16 +1,37 @@
 public class SavingsAccount {
 
-  private long total = 0;
+    private long total = 0;
+    private final int accountNumber;
 
-  public boolean withdraw(long amount) {
-      
-  }
+    public SavingsAccount() {
+        accountNumber = hashCode();
+    }
 
-  public void deposit(long amount) {
+    public synchronized boolean withdraw(long amount) {
+        if (total >= amount) {
+            total -= amount;
+            System.out.println("Withdrew $" + amount + " from account " + accountNumber);
+            return true;
+        } else {
+            System.out.println("Could not withdraw $" + amount + " since the balance of account " + accountNumber+ " is $" + total);
+            return false;
+        }
+    }
 
-  }
+    public synchronized void deposit(long amount) {
+        total += amount;
+        System.out.println("Deposited $" + amount + ". New account balance for " + accountNumber + " is $" + total);
+    }
 
-  public long getTotal() {
-      return total;
-  }
+    public synchronized long getTotal() {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "SavingsAccount{" +
+                "accountNumber=" + accountNumber +
+                "," + "total=$" + total +
+                '}';
+    }
 }
